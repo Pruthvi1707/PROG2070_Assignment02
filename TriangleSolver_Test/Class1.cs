a3fbca8328cfc7e4c0389e72fa2543c2f04f6b0b
Pruthvi1707 <102315629+Pruthvi1707@users.noreply.github.com>
2023-02-10 12:33:04 -0500

    This is my first Commit

--- TriangleSolver_Test/Class1.cs
+++ TriangleSolver_Test/Class1.cs
@@ -29,6 +29,53 @@ namespace TriangleSolver_Test
 			Assert.AreEqual(expected, actual);
 		}
 
+		//Three (3) tests for a valid isosceles triangle
+
+		//(01)
+		[Test]
+		public void Isosceles_Side5And5And6_Valid_Test01()
+		{
+			Triangle c1 = new Triangle();
+
+			int f_side = 5;
+			int s_side = 5;
+			int t_side = 6;
+
+			string expected = "Based on two sides being equal, the type of triangle is an ISOSCELES";
+			string actual = c1.AnalyzeTriangle(f_side, s_side, t_side);
+			Assert.AreEqual(expected, actual);
+		}
+
+		//(02)
+		[Test]
+		public void Isosceles_Side7And6And6_Valid_Test02()
+		{
+			Triangle c1 = new Triangle();
+
+			int f_side = 7;
+			int s_side = 6;
+			int t_side = 6;
+
+			string expected = "Based on two sides being equal, the type of triangle is an ISOSCELES";
+			string actual = c1.AnalyzeTriangle(f_side, s_side, t_side);
+			Assert.AreEqual(expected, actual);
+		}
+
+		//(03)
+		[Test]
+		public void Isosceles_Side7And8And7_Valid_Test03()
+		{
+			Triangle c1 = new Triangle();
+
+			int f_side = 7;
+			int s_side = 8;
+			int t_side = 7;
+
+			string expected = "Based on two sides being equal, the type of triangle is an ISOSCELES";
+			string actual = c1.AnalyzeTriangle(f_side, s_side, t_side);
+			Assert.AreEqual(expected, actual);
+		}
+
 
 
 	}
